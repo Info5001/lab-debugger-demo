5e2c646815cd20674425395f1be851bcee85c3eb
archil-nu <alelashvili@coe.neu.edu>
2023-02-10 19:25:56 -0500

    Debugger demo complete

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -3,12 +3,18 @@ package model;
 public class Ball {
     String color;
 
+    public Ball(String color) {
+        System.out.println("We have created a new instance of a ball with color: " + color );
+        this.color = color;
+    }
+
 
     public String getColor() {
         return color;
     }
 
     public void setColor(String color) {
+        System.out.println("We are setting the color to: " + color );
         this.color = color;
     }
 
--- src/ui/debuggerDemo.java
+++ src/ui/debuggerDemo.java
@@ -5,31 +5,17 @@ import model.Ball;
 public class debuggerDemo {
     public static void main(String[] args) {
 
+        Ball blue = new Ball("blue");
+        Ball red = blue;
+        Ball green = red;
+        
+        blue.setColor("blue");
+        red.setColor("red");
+        green.setColor("green");
 
-
-
-        int a = 10;
-        int b = 5;
-       
-        int c = a;
-        b = c;
-        a = 100;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("c = " + c);
-
-        // Ball blue = new Ball();
-        // Ball red = blue;
-        // Ball green = red;
-
-        // blue.setColor("blue");
-        // red.setColor("red");
-        // green.setColor("green");
-
-        // blue.print();
-        // red.print();
-        // green.print();
+        blue.print();
+        red.print();
+        green.print();
 
 
 
